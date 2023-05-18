9ef23f5272f2cbdd3094b8ea4f69e631d2e33e5c
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2023-05-17 23:06:14 -0500

    add enable cors

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import  graphql from '@rollup/plugin-graphql'
+import graphql from '@rollup/plugin-graphql'
 
 
 // https://vitejs.dev/config/
@@ -11,6 +11,16 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    cors: {
+      origin: "https://talachapp-api-47f5p.ondigitalocean.app/talachapp-api2/graphql",
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: true,
+      allowedHeaders: 'Content-Type, Authorization, X-Requested-With',
     }
   }
 })
